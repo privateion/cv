d8f3479fad2264edcb9f254ebc4d5d4aaefc8aef
divineion <najima.guermoudi@gmail.com>
2024-10-09 14:05:41 +0200

    cv/src/App.vue: remove unused import

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import AppFooter from './components/AppFooter.vue';
 import AppHeader from "./components/AppHeader.vue";
 </script>
